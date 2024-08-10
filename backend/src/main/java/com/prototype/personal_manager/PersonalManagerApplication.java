b4c94d8a76b30659288f150ef213f537353b3fff
yohagos <yosef.hagos@googlemail.com>
2024-08-10 11:32:39 +0200

    init backend to repository

---
+++ backend/src/main/java/com/prototype/personal_manager/PersonalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.personal_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalManagerApplication.class, args);
+	}
+
+}
