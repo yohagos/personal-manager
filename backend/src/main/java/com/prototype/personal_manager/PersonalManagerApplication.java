cbc27bb63ced35742673bef371dd4da4bf408a38
yohagos <yosef.hagos@googlemail.com>
2024-08-11 15:27:45 +0200

    feat: modified application.yaml, added annotations to main class

--- backend/src/main/java/com/prototype/personal_manager/PersonalManagerApplication.java
+++ backend/src/main/java/com/prototype/personal_manager/PersonalManagerApplication.java
@@ -2,8 +2,12 @@ package com.prototype.personal_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 public class PersonalManagerApplication {
 
 	public static void main(String[] args) {
